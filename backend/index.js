9d5b8120a4d72df415b9a89202befff910836c4e
claudiotorancio@gmail.com <claudiotorancio@gmail.com>
2023-09-27 12:02:38 -0300

    11 commit

--- backend/index.js
+++ backend/index.js
@@ -35,7 +35,7 @@ app.use(express.json());
 app.use(cors())
 
 
-app.get('/api/books', require('../api/books.js'))
+app.use('/api/books', require('./routes/books.js'))
 
 
  
--- backend/routes/books.js
+++ backend/routes/books.js
@@ -5,7 +5,7 @@ const router = Router();
 const {unlink} = require('fs-extra')
 const path = require('path')
 
-const Book = require('../backend/models/Book.js')
+const Book = require('../models/Book.js')
 
 router.get('/', async (req, res) => {
     const books = await Book.find()
